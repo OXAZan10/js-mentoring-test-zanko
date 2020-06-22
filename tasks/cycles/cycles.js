94922eb934c722d93979e2ae23be7620d25ef71a
Aksana Zanko <Aksana_Zanko@epam.com>
2020-06-22 14:40:31 +0300

    cycles

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -13,8 +13,19 @@
 function calculateDiscount(redemption) {
   let discount;
 
-  //PLACE YOUR CODE HERE
-
+  while (redemption >= 0 || redemption <= 9999) {
+    if (redemption >= 0 || redemption <= 350) {
+      discount = 0;
+    } else if (redemption > 350 || redemption <= 1350) {
+      discount = 15;
+    } else if (redemption > 1350 || redemption <= 2700) {
+      discount = 30;
+    } else if (redemption > 2700 || redemption <= 6500) {
+      discount = 45;
+    } else {
+      discount = 0;
+    }
+  }
   return discount;
 }
 
@@ -23,6 +34,31 @@ function calculateDiscount(redemption) {
  */
 {
   const i = 10; //10! = 3628800
+  let forFactorial = 1;
+  let whileFactorial = 1;
+  let j = 1;
+  let doWhileFactorial = 1;
+  let k = 1;
+  //============================================
+  for (let j = 1; j <= i; j++) {
+    forFactorial *= j;
+  }
+  console.log(forFactorial);
+  //============================================= 
+  while (j <= i) {
+    whileFactorial *= j;
+    j++;
+  }
+  console.log(whileFactorial);
+  //=============================================
+  do {
+    doWhileFactorial *= k;
+    k++;
+
+  }
+  while (k <= i);
+  console.log(doWhileFactorial);
+
 }
 
 /**
@@ -30,6 +66,12 @@ function calculateDiscount(redemption) {
  */
 {
   const substr = ["I", " love", " JS"];
+  let result = [];
+  for (let i = 0; i < substr.length; i++) {
+    result += substr[i]
+  }
+
+  console.log(result);
 }
 
 /**
@@ -42,6 +84,12 @@ function calculateDiscount(redemption) {
     interestOnDeposit: 250,
     otherExpences: -300
   };
+  let totalIncome=0;
+  for (let summ in personIncomes)
+  {
+    totalIncome+=personIncomes[summ];
+  }
+  console.log(totalIncome);
 }
 
 module.exports = calculateDiscount;
