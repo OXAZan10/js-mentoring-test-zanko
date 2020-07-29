6517dfb0b4b74e363b13096e645f4ef6be097617
Aksana Zanko <Aksana_Zanko@epam.com>
2020-07-29 10:00:29 +0300

    HW_8_async

--- tasks/async/async.js
+++ tasks/async/async.js
@@ -4,41 +4,59 @@ const {
     callbackFunctionError,
     promiseFunctionError
 } = require("./async_api");
+const { assert } = require("chai");
 
 /**
  * console.log data got from callbackFunction
  */
 function printDataCallback() {
+    function printTextCallback(err, data){
+        console.log (data);
+    }
+    callbackFunction(printTextCallback);   
 }
 
 /**
  * console.log data got from promiseFunction
  */
 function printDataPromise() {
+    promiseFunction().then(response => console.log(response))
 }
-
 /**
  * console.log data got from promiseFunction using async/await
  */
 async function printDataAsyncAwait() {
+    console.log (await promiseFunction());
 }
 
 /**
  * throw error data from callbackFunctionError
  */
 function handleErrorCallback() {
+    function printErrCallback(error){
+        throw error;
+    }
+    callbackFunctionError(printErrCallback);  
 }
 
 /**
  * throw error come from promiseFunctionError
  */
 function handlePromiseError() {
+    promiseFunctionError.catch(function(error) {
+        throw error;
+    });
 }
 
 /**
  * throw error come from promiseFunctionError using async/await
  */
 async function handleAsyncAwaitError() {
+    try {
+        await promiseFunctionError();
+      } catch (error) {
+        throw error;
+      }
 }
 
 module.exports = {
